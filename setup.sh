134cd35c4d1d97c482350bbc0aa22379196fe892
yukinon874 <yuki.nakamura.r1@dc.tohoku.ac.jp>
2022-12-08 02:16:18 +0900

    update

--- setup.sh
+++ setup.sh
@@ -5,7 +5,6 @@ apt-get install -y --no-install-recommends \
     ca-certificates \
     jq \
     wget \
-    python3-dev\
     vim \
     curl \
     gcc
@@ -13,9 +12,9 @@ apt-get install -y --no-install-recommends \
 
 mkdir -p data work models
 
-#### TODO: add ####
 pip install --upgrade pip
 pip install --user -r requirements.txt
 
-#wget -P data https://jaqket.s3.ap-northeast-1.amazonaws.com/data/aio_02/aio_02_train.jsonl
-#wget -P data https://jaqket.s3.ap-northeast-1.amazonaws.com/data/aio_03/aio_03_test_unlabeled.jsonl
+wget -nc -P data https://jaqket.s3.ap-northeast-1.amazonaws.com/data/aio_02/aio_02_train.jsonl
+wget -nc -P data https://jaqket.s3.ap-northeast-1.amazonaws.com/data/aio_02/aio_02_dev_v1.0.jsonl
+wget -nc -P data https://jaqket.s3.ap-northeast-1.amazonaws.com/data/aio_03/aio_03_test_unlabeled.jsonl
