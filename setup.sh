dba1a70fbaa4a9e80e99d7bfa10efa910496670e
yukinon874 <yuki.nakamura.r1@dc.tohoku.ac.jp>
2022-12-12 12:58:05 +0900

    udpate

--- setup.sh
+++ setup.sh
@@ -9,14 +9,16 @@ sudo apt-get install -y --no-install-recommends \
     curl \
     gcc
 
-mkdir -p data work models
+mkdir -p work models
 
 pip install --upgrade pip
 pip install --user -r requirements.txt
-pip install git+https://github.com/huggingface/transformers.git                                   
-pip install accelerate
-pip install bitsandbytes
+
+# if you need load_in_8bit
+#pip install git+https://github.com/huggingface/transformers.git                                   
+#pip install accelerate
+#pip install bitsandbytes
 
 # Download data
-wget -nc -O data/dev.jsonl https://jaqket.s3.ap-northeast-1.amazonaws.com/data/aio_02/aio_02_train.jsonl
-wget -nc -O data/test.jsonl https://jaqket.s3.ap-northeast-1.amazonaws.com/data/aio_02/aio_02_dev_unlabeled_v1.0.jsonl
+#wget -nc -O data/dev.jsonl https://jaqket.s3.ap-northeast-1.amazonaws.com/data/aio_02/aio_02_train.jsonl
+#wget -nc -O data/test.jsonl https://jaqket.s3.ap-northeast-1.amazonaws.com/data/aio_02/aio_02_dev_unlabeled_v1.0.jsonl
