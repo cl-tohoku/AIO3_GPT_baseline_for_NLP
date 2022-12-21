a107750ab773fc028e70a9dba653d1dd3bfc21bd
Yuki Nakamura <60589029+yukinon874@users.noreply.github.com>
2022-12-22 07:49:00 +0900

    Update eval_model_jsonl.py

--- eval_model_jsonl.py
+++ eval_model_jsonl.py
@@ -218,6 +218,7 @@ if __name__ == "__main__":
     parser.add_argument("--lang",
                         type=str,
                         default="ja",
+                        choices=["ja", "en"],
                         help="Language [ja,en]")
     parser.add_argument("--debug",
                         action="store_true",
