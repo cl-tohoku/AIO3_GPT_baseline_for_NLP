707397302df6aeaf9ebbbc549771fc1e9315f48f
jnszk <hi.jk.lm.23@gmail.com>
2022-12-21 22:08:12 +0900

    clean up util.py

--- util.py
+++ util.py
@@ -18,25 +18,6 @@ def add_prompt(question: str, lang: str, **kwargs: dict[str, Any]) -> str:
     return prompt
 
 
-
-        #prompt = f"Answer this question: {question}? The answer is ["
-#        prompt = f'''
-#        Share of export from India is the maximum to the following country? [United States]
-#        Which structure in the urinary system carries urine to the bladder? [ureters]
-#        Who won Americas next top model season 22? [Nyle DiMarco]
-#        {question}? [''' 
-
-
-        #prompt = f'質問：{question}の答えは「'    
-
-    #prompt = f'質問：「ちょっと何言ってるか分からない」というセリフで知られる、お笑いコンビ・サンドウィッチマンのメンバーは誰?の答えは【富澤たけし】</s>質問：{question}の答えは「'
-    #prompt = f'質問：「ちょっと何言ってるか分からない」というセリフで知られる、お笑いコンビ・サンドウィッチマンのメンバーは誰?の答えは「富澤たけし」</s>質問：{question}の答えは「'
-    #prompt = f'質問：『ちょっと何言ってるか分からない』というセリフで知られる、お笑いコンビ・サンドウィッチマンのメンバーは誰?の答えは「富澤たけし」</s>質問：{question}の答えは「'    
-    #prompt = f'質問：質問：スペイン領バレアレス諸島に属し、ダンスミュージックが盛んなことから『パーティ・アイランド』とも呼ばれる島はどこ?の答えは「イビサ島」</s>質問：『ちょっと何言ってるか分からない』というセリフで知られる、お笑いコンビ・サンドウィッチマンのメンバーは誰?の答えは「富澤たけし」</s>質問：{question}の答えは「'
-
-
-
-
 def extract_answer(output: str, lang: str) -> str:
     if lang == "en":
         return re.findall("\[(.*?)\]", output)[-1] # capture []
