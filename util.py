67a3bf33a1e9670ebcc5606fa54104227e688e7e
jnszk <jnszk@github>
2022-12-21 16:29:56 +0000

    add comments in util.py

--- util.py
+++ util.py
@@ -6,6 +6,9 @@ from typing import Any
 '''
 Input: question
 Output: prompt
+Note:
+    ・The model's answer is the content of the model's output 「」(Japanese) or [ ] (English)
+    ・So, it is better to end the prompt with "「" (Japanese) or "[" (english)
 '''
 def add_prompt(question: str, lang: str, **kwargs: dict[str, Any]) -> str:
     # Add your prompt
@@ -18,6 +21,12 @@ def add_prompt(question: str, lang: str, **kwargs: dict[str, Any]) -> str:
     return prompt
 
 
+'''
+Input: model prediction str
+Output: answer str
+Note:
+    ・You can also change this part if you want.
+'''
 def extract_answer(output: str, lang: str) -> str:
     if lang == "en":
         return re.findall("\[(.*?)\]", output)[-1] # capture []
