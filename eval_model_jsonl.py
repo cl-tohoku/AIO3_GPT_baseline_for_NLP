2e445ab500b226295da4386eccee779c10a40e57
jnszk <jnszk@github>
2022-12-21 00:35:02 +0000

    modified eval_model_json.py

--- eval_model_jsonl.py
+++ eval_model_jsonl.py
@@ -1,110 +1,142 @@
 import pandas as pd
-import re
 import torch
 from transformers import T5Tokenizer, AutoModelForCausalLM, AutoTokenizer, AutoModel
 from tqdm import tqdm
 import argparse
 import os
-from util import add_prompt
+
+from util import add_prompt, extract_answer
 import accelerate
+import logging
+
+
+logger = logging.getLogger(__name__)
+fmt = "%(asctime)s %(levelname)s %(name)s : %(message)s"
+logging.basicConfig(level=logging.INFO, format=fmt)
+
 
 def main(args):
     ### load pretrained model ###
-    model_path = './models/japanese-gpt.pt'
-    tokenizer_path = './models/japanese-gpt_tokenizer'
-    if args.english_ver:
+    if args.lang == "en":
         model_path = './models/english-gpt.pt'
         tokenizer_path = './models/english-gpt_tokenizer'
-
-    if os.path.exists(model_path) and os.path.exists(tokenizer_path):
-        print("model loading via offline...")
-        tokenizer = AutoTokenizer.from_pretrained(tokenizer_path)
-        model = AutoModelForCausalLM.from_pretrained(
-            model_path,
-            torch_dtype=torch.float16,
-            #load_in_8bit=True,
-            device_map="auto",
-        )
+    elif args.lang == "ja":
+        model_path = './models/japanese-gpt.pt'
+        tokenizer_path = './models/japanese-gpt_tokenizer'
     else:
-        print("model loading via online...")
-        if args.english_ver:
-            # https://huggingface.co/togethercomputer/GPT-JT-6B-v1
-            tokenizer = AutoTokenizer.from_pretrained("togethercomputer/GPT-JT-6B-v1")
-            model = AutoModelForCausalLM.from_pretrained("togethercomputer/GPT-JT-6B-v1",
-            load_in_8bit=True,
-            device_map="auto")
-        else:
+        assert 0, args.lang
+
+        
+    if not args.force_load_model and os.path.exists(model_path) and os.path.exists(tokenizer_path):
+        logger.info("model loading via offline...")
+        t_tkn = tokenizer_path
+        t_mdl = model_path
+    else:
+        logger.info("model loading via online...")
+        if args.lang == "en":
+            # https://huggingface.co/gpt2-xl
+            t_tkn = "gpt2-xl" #"EleutherAI/gpt-neo-1.3B" #"neulab/gpt2-finetuned-wikitext103" #"gpt2-xl"
+            t_mdl = "gpt2-xl" #"EleutherAI/gpt-neo-1.3B" #"neulab/gpt2-finetuned-wikitext103" #"gpt2-xl"
+        elif args.lang == "ja":
             # https://huggingface.co/rinna/japanese-gpt-1b
-            tokenizer = AutoTokenizer.from_pretrained("rinna/japanese-gpt-1b")
-            model = AutoModelForCausalLM.from_pretrained(
-                "rinna/japanese-gpt-1b",
-                torch_dtype=torch.float16,
-                #load_in_8bit=True,
-                device_map="auto",
-            )
+            t_tkn = "rinna/japanese-gpt-1b"
+            t_mdl = "rinna/japanese-gpt-1b"
+        else:
+            assert 0, args.lang
+    tokenizer = AutoTokenizer.from_pretrained(t_tkn)
+    model = AutoModelForCausalLM.from_pretrained(
+        t_mdl,
+        torch_dtype=torch.float16,
+        #load_in_8bit=True,
+        device_map="auto",
+    )
+
     if args.save_model:
-        print("saving model to local.")
+        logger.info("saving model to local.")
         tokenizer.save_pretrained(tokenizer_path)
         model.save_pretrained(model_path, max_shard_size="500MB")
 
     if torch.cuda.is_available():
         model = model.to("cuda")
-    print("INFO: The model was loaded.")
+    logger.info("The model was loaded.")
 
 
     ### load data ###
-    print("loading data...")
+    logger.info("loading data...")
     data = pd.read_json(args.input_file, lines=True)
     if args.sample > 0:
-        data = data.sample(n=args.sample, random_state=1)
 
+        data = data.sample(n=args.sample, random_state=42)
+
+    qid = list(data["qid"])
     texts = list(data["question"])
-    answers = list(data["answers"])
-    model_answers = []
+    if "answers" in data:
+        answers = list(data["answers"])
+    else:
+        answers = None
+    predictions = []
     output_list = []    
     max_length = 100 
 
-
-    print("start estimation...")
+    logger.info("start estimation...")
     ### predict answer ###
     for text in tqdm(texts, total=len(texts)):
-        text = add_prompt(text) # add your own prompt to question
+        text = add_prompt(text, args.lang) # add your own prompt to question
         token_ids = tokenizer.encode(
             text, add_special_tokens=False, return_tensors="pt")
         model.eval()
         with torch.no_grad():
-            output_ids = model.generate(
-                token_ids.to(model.device),
-                max_length=len(token_ids[0])+max_length,
-                min_length=1,
-                do_sample=False,
-                top_k=500,
-                top_p=0.95,
-                pad_token_id=tokenizer.pad_token_id,
-                bos_token_id=tokenizer.bos_token_id,
-                eos_token_id=tokenizer.eos_token_id,
-                bad_word_ids=[[tokenizer.unk_token_id]]
-            )
-
+            if args.lang == "en":
+                output_ids = model.generate(
+                    token_ids.to(model.device),
+                    max_length=len(token_ids[0])+max_length,
+                    min_length=1,
+                    do_sample=False,
+                    top_k=500,
+                    top_p=0.95,
+                    pad_token_id=tokenizer.pad_token_id,
+                    #bos_token_id=tokenizer.bos_token_id,
+                    #eos_token_id=tokenizer.eos_token_id,
+                )
+            elif args.lang == "ja":
+                output_ids = model.generate(
+                    token_ids.to(model.device),
+                    max_length=len(token_ids[0])+max_length,
+                    min_length=1,
+                    do_sample=False,
+                    #top_k=500,
+                    #top_p=0.95,
+                    pad_token_id=tokenizer.pad_token_id,
+                    bos_token_id=tokenizer.bos_token_id,
+                    eos_token_id=tokenizer.eos_token_id,
+                )
+            else:
+                assert 0, args.lang
         output = tokenizer.decode(output_ids.tolist()[0])
+        logger.debug(f"debug: {answers[len(predictions)]} ||| {output}")
+        print(f"debug: {answers[len(predictions)]} ||| {output}")
         try:
-            model_ans = re.findall("「(.*?)」", output)[-1] # capture 「」
-            #model_ans = re.findall("【(.*?)】", output)[-1] # capture 「」
+            model_ans = extract_answer(output, args.lang)
         except IndexError:
             model_ans = output
-            print("longer output:", output)
+            logger.warning(f"longer output: {output}")
         output_list.append(output)
-        model_answers.append(model_ans)
-    assert len(model_answers) == len(answers), f"Model output does not match input data count."
-    correct = [1 if result in gold else 0 for result,
-               gold in zip(model_answers, answers)]
-    correct_num = correct.count(1)
-    acc = correct_num/len(model_answers)
-    print(f"Number of questions:{len(answers)}")
-    print(f"Number of correct answers:{correct_num}")
-    print(f"Accuracy rate:{acc:.3f}")
-    df_ans = pd.DataFrame([texts, answers, model_answers, correct, output_list], index=[
-        "question", "answers", "prediction", "correct", "output"]).T
+        predictions.append(model_ans)
+    if answers is not None:
+        assert len(predictions) == len(answers), f"Model output does not match input data count."
+        correct = [1 if result in gold else 0 for result,
+                   gold in zip(predictions, answers)]
+        correct_num = correct.count(1)
+        acc = correct_num/len(predictions)
+        print(f"Number of questions:{len(answers)}")
+        print(f"Number of correct answers:{correct_num}")
+        print(f"Accuracy rate:{acc:.3f}")
+        df_ans = pd.DataFrame([qid, texts, answers, predictions, correct, output_list], index=[
+            "qid", "question", "answers", "prediction", "correct", "output"]).T
+    else:
+        df_ans = pd.DataFrame([qid, texts, predictions, output_list], index=[
+            "qid", "question", "prediction", "output"]).T
+
     df_ans.to_csv(args.output_file)
 
 
@@ -128,9 +160,13 @@ if __name__ == "__main__":
     parser.add_argument("--save_model",
                         action="store_true",
                         help="If true, save GPT model in local environment.")
-    parser.add_argument("--english_ver",
+    parser.add_argument("--force_load_model",
                         action="store_true",
-                        help="If true, use english GPT model.")
+                        help="If true, load model and tokenizer from online.")    
+    parser.add_argument("--lang",
+                        type=str,
+                        default="ja",
+                        help="Language [ja,en]")
     args = parser.parse_args()
 
     main(args)
--- util.py
+++ util.py
@@ -1,23 +1,46 @@
 from __future__ import annotations
+import re
 from typing import Any
+
 
 '''
 Input: question
 Output: prompt
-Note:
-    ・The model's answer is the content of the model's output 「」
-    ・So, it is better to end the prompt with 「 
 '''
-def add_prompt(question: str, **kwargs: dict[str, Any]) -> str:
+def add_prompt(question: str, lang: str, **kwargs: dict[str, Any]) -> str:
     # Add your prompt
-    #prompt = f'{question}の答えは「'
-    #prompt = f'質問：{question}の答えは「'    
+    if lang == "en":
+        prompt = f"Answer this question: {question}? Answer: ["
+    elif lang == "ja":
+        f'{question}の答えは「'
+    else:
+        assert 0, lang
+    return prompt
+
+
+
+        #prompt = f"Answer this question: {question}? The answer is ["
+#        prompt = f'''
+#        Share of export from India is the maximum to the following country? [United States]
+#        Which structure in the urinary system carries urine to the bladder? [ureters]
+#        Who won Americas next top model season 22? [Nyle DiMarco]
+#        {question}? [''' 
+
+
+        #prompt = f'質問：{question}の答えは「'    
 
     #prompt = f'質問：「ちょっと何言ってるか分からない」というセリフで知られる、お笑いコンビ・サンドウィッチマンのメンバーは誰?の答えは【富澤たけし】</s>質問：{question}の答えは「'
     #prompt = f'質問：「ちょっと何言ってるか分からない」というセリフで知られる、お笑いコンビ・サンドウィッチマンのメンバーは誰?の答えは「富澤たけし」</s>質問：{question}の答えは「'
     #prompt = f'質問：『ちょっと何言ってるか分からない』というセリフで知られる、お笑いコンビ・サンドウィッチマンのメンバーは誰?の答えは「富澤たけし」</s>質問：{question}の答えは「'    
     #prompt = f'質問：質問：スペイン領バレアレス諸島に属し、ダンスミュージックが盛んなことから『パーティ・アイランド』とも呼ばれる島はどこ?の答えは「イビサ島」</s>質問：『ちょっと何言ってるか分からない』というセリフで知られる、お笑いコンビ・サンドウィッチマンのメンバーは誰?の答えは「富澤たけし」</s>質問：{question}の答えは「'
-    prompt = f'質問：クォークと反クォークからなり、湯川秀樹が理論的に存在を予言し、ノーベル物理学賞受賞にいたった粒子は何?の答えは「中間子」</s>質問：質問：スペイン領バレアレス諸島に属し、『パーティ・アイランド』とも呼ばれる島はどこ?の答えは「イビサ島」</s>質問：『ちょっと何言ってるか分からない』というセリフで知られる、お笑いコンビ・サンドウィッチマンのメンバーは誰?の答えは「富澤たけし」</s>質問：{question}の答えは「'
 
 
-    return prompt
+
+
+def extract_answer(output: str, lang: str) -> str:
+    if lang == "en":
+        return re.findall("\[(.*?)\]", output)[-1] # capture []
+    elif lang == "ja":
+        return re.findall("「(.*?)」", output)[-1] # capture 「」
+    else:
+        assert 0, lang
